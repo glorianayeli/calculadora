bc7c1ce7fc114011726b37c4bab8115738cf45be
glorianayeli <glorianayeli_jaime@ucol.mx>
2019-03-13 10:35:40 -0600

    modificacion de condiciones

--- calculadora.c
+++ calculadora.c
@@ -1,7 +1,14 @@
 #include <18F4620.h>
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
-#fuses NOPBADEN, MCLR, STVREN, NOLVP, NODEBUG
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
+#use fast_io(a)
+#use fast_io(b) 
+#use fast_io(c) 
+#use fast_io(d)
+
+void resultado13bits(int16 resultado);
+void error();
 
 //#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
 
@@ -11,28 +18,68 @@
    #use fast_io(c)
 #endif
 
-void main (void){
-   setup_oscillator(OSC_16MHZ);
-#ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
-   printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
-#endif
-   set_tris_d(0xff);
-   set_tris_c(0xff);
-   set_tris_b(0xf0);
-   set_tris_a(0x00);
-   set_tris_e(0x0);
+void main (void)
+{    
+    setup_oscillator(OSC_16MHZ);
+    #ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
+        printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
+    #endif
+    set_tris_d(0xff);
+    set_tris_c(0xff);
+    set_tris_b(0xf0);
+    set_tris_a(0x00);
+    set_tris_e(0x0);
    
-   int8 operacion = input_b();
-   int8 numerouno = input_c();
-   int8 numerodos = input_d();
-   int16 resultado;
-   if(input(PIN_B7)==1)
+    int8 numerouno = input_c();
+    int8 numerodos = input_d();
+    int16 resultado;
+    if(input(PIN_B7)==1)
+    {
+        //sumas sucesivas y metodos de registros, dezplazamiento de registros
+        delay_ms(40);//evitar rebote
+        resultado = numerouno+numerodos;
+        resultado13bits(resultado);
+   } 
+   else if(input(PIN_B6)==1)
    {
+       //sumas sucesivas y metodos de registros, dezplazamiento de registros
        delay_ms(40);//evitar rebote
-       resultado = numerouno+numerodos;
-       output_a(resultado);
-       output_b(resultado>>7);
-       output_e(resultado<<10);
+       resultado = numerouno-numerodos;
+       resultado13bits(resultado);
    }
-   delay_ms(500);
+   else if(input(PIN_B5)==1)
+   {
+       delay_ms(40);
+       resultado=numerouno*numerodos;
+       if(resultado>4096)
+       {
+           error();
+       }
+   }
+   else if(input(PIN_B4)==1)
+   {
+        delay_ms(40);//rebote de botón
+        if(numerodos>0)
+        {
+            resultado=numerouno/numerodos;
+            resultado13bits(resultado);
+        }
+        else
+        {
+            error();
+        }
+   }
 }
+void resultado13bits (int16 resultado)
+{
+    output_a(resultado);
+    output_b(resultado>>6);
+    output_e(resultado>>10);
+}
+void error()
+{
+    output_a(0b111111);
+    output_b(0xf);
+    output_e(0b111);
+}
+   
